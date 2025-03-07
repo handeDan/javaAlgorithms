fe9b6188b9c1a80539e54440d107225e2362d7b0
hande <hande.dann@gmail.com>
2025-03-07 11:32:25 +0300

    findMissingNumber

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    static int findMissingNumber(int [] numbers) {
+        //problem: ardışık olan sayıda eksik olan sayıyı bulalım. artış miktarı bu soruda her zaman 1(ardışık sayı).
+        //input: [1,2,3,5,6,7,...] n elemanlı bir input. output: 4
+        int missingNumber = -1; //missingNumber yoksa -1 olsun diye verdik.
+
+        for(int i = 0; i<numbers.length-1; i++) {
+            if(!(numbers[i+1] == numbers[i] + 1)) {
+                missingNumber = numbers[i] + 1;
+                break;
+            }
+        }
+        return missingNumber;
+    }
+    //ALTERNATIVE SOLVE-1: toplam üzerinden bir çözüm de olabilir.
+    //ALTERNATIVE SOLVE-2: [1,2,3,5,6]  length=5
+    //normalde length+1 kadar uzunlukta olmalıydı
+    //bu ikisi kıyaslanabilir
+}
